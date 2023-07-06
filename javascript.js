0fa1fc1fad46569c6dbd50a5a6b2934f66d4d52a
Pegahz <zahra.javadipour1@gmail.com>
2023-07-06 05:39:28 -0700

    Edit js file and add comments

--- javascript.js
+++ javascript.js
@@ -1,71 +1,50 @@
+//This function returns the computer's choice
 function getComputerChoice() {
-    const choises = ['Rock', 'Paper', 'Scissors'];
+    const choices = ['Rock', 'Paper', 'Scissors'];
     let index = Math.floor(Math.random() * 3);
-    return choises[index];
+    return choices[index];
 }
 
+//This function returns the player's choice in a standard format
+function standardPlayerSelection(playerSelection) {
+    return playerSelection[0].toUpperCase() + playerSelection.substr(1).toLowerCase();
+}
+
+//This function plays one round of the game
 function playRound(playerSelection, computerSelection) {
-    let message;
-    let playerChoise = playerSelection[0].toUpperCase() + playerSelection.substr(1).toLowerCase();
-    console.log(playerChoise);
+    console.log(playerSelection);
     console.log(computerSelection);
-    if (computerSelection === 'Rock') {
-        if (playerChoise === 'Rock') {
-            message = "Draw! Rock doesn't beat Rock";
-            console.log(message);
-            return 0;
-        }
-        if (playerChoise === 'Paper') {
-            message = "You Win! Paper beats Rock";
-            console.log(message);
-            return 1;
-        }
-        if (playerChoise === 'Scissors') {
-            message = "You Lose! Rock beats Scissors";
-            console.log(message);
-            return -1;
-        }
+
+    //How the plaer wins
+    if (computerSelection === 'Rock' && playerSelection === 'Paper' ||
+        computerSelection === 'Paper' && playerSelection === 'Scissors' ||
+        computerSelection === 'Scissors' && playerSelection === 'Rock') 
+        {
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+        return 1;
     }
-    if (computerSelection === 'Paper') {
-        if (playerChoise === 'Rock') {
-            message = "You Lose! Paper beats Rock";
-            console.log(message);
-            return -1;
-        }
-        if (playerChoise === 'Paper') {
-            message = "Draw! Paper doesn't beat Paper";
-            console.log(message);
-            return 0;
-        }
-        if (playerChoise === 'Scissors') {
-            message = "You Win! Scissors beat Paper";
-            console.log(message);
-            return 1;
-        }
+
+    //How the computer wins
+    if (computerSelection === 'Rock' && playerSelection === 'Scissors' ||
+        computerSelection === 'Paper' && playerSelection === 'Rock' ||
+        computerSelection === 'Scissors' && playerSelection === 'Paper') 
+        {
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+        return -1;
     }
-    if (computerSelection === 'Scissors') {
-        if (playerChoise === 'Rock') {
-            message = "You Win! Rock beats Scissors";
-            console.log(message);
-            return 1;
-        }
-        if (playerChoise === 'Paper') {
-            message = "You Lose! Scissors beat Paper";
-            console.log(message);
-            return -1;
-        }
-        if (playerChoise === 'Scissors') {
-            message = "Draw! Scissors don't beat Scissors";
-            console.log(message);
-            return 0;
-        }
+
+    //A draw
+    if (computerSelection === playerSelection) {
+        console.log(`Draw! ${computerSelection} can't beat ${playerSelection}`);
+        return 0;
     }
 }
 
+//This function plays 5 round of the game and after counting the scores detemines who wins
 function game() {
     let score, playerScore = 0, computerScore = 0;
     for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Rock, Paper or Scissors?");
+        const playerSelection = standardPlayerSelection(prompt("Rock, Paper or Scissors?"));
         const computerSelection = getComputerChoice();
         score = playRound(playerSelection, computerSelection);
         (score === 1) ? playerScore++ : (score === -1) ? computerScore++ : null;
